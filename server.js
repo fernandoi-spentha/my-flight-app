9dc58bc3c03c03d6713e4bf71f1f7970e41a5729
fernandoi-spentha <fernando.i@spentha.com>
2026-02-11 12:40:00 +0100

    Update server.js

--- server.js
+++ server.js
@@ -87,5 +87,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(PORT, '0.0.0.0', () => {
-  console.log('MyFlight running on port ' + PORT);
+  console.log('MyFlight v2 running on port ' + PORT);
 });
